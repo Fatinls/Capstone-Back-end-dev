ac8468a4ccd09abd91754ef90562f7a753a667f8
Fatinls <91710776+Fatinls@users.noreply.github.com>
2023-05-20 01:14:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,14 +13,11 @@ const page404NotFound = require('./middleware/handler404NotFound');
 var app = express();
 require('dotenv').config();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 // cara cors lain
