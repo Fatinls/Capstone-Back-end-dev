da6fbe83c54197665ee82f696bae2fef301d537a
Fatinls <123200071@student.upnyk.ac.id>
2023-05-20 17:43:54 +0700

    update multer

--- utils/multerImage.js
+++ utils/multerImage.js
@@ -12,9 +12,6 @@ const storage = multer.memoryStorage({
 });
 const uploadImage = multer({
   storage: multer.memoryStorage(),
-  limits: {
-    fileSize: 500 * 1024,
-  },
 });
 
 module.exports = uploadImage;
